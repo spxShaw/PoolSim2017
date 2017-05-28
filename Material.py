d742d55f2de133c63951fa7bc28b8c849fc764cf
spxShaw <sshaw32@gatech.edu>
2017-05-28 19:28:59 -0400

    Added definition for Table and Material

---
+++ Material.py
@@ -0,0 +1,5 @@
+class Material:
+
+    def __init__(self, friction_coefficient, restitution_coefficient):
+        self.friction_coefficient = friction_coefficient
+        self.restitution_coefficient = restitution_coefficient
---
+++ Table.py
@@ -0,0 +1,6 @@
+class Table:
+
+    def __init__(self, material, length, width):
+        self.material = material
+        self.length = length
+        self.width = width
